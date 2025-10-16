d847a499fccc3cb49f487c58f29bce947498a9e1
ANIRBAN SANTRA <129375292+anirbansantra748@users.noreply.github.com>
2025-10-16 09:33:43 +0530

    resolve conflicts

--- a.js
+++ a.js
@@ -1,1 +1,22 @@
-echo "var x=1;console.log(x);var y=2" > test.js
+const users = ["Anirban", "Charu", "Oni"]
+
+function greet(name) {
+  console.log("Hi " + name)
+  return "Hello " + name
+}
+
+let result = greet(user)
+
+for(let i = 0; i < users.length; i++) {
+  console.log("User: " + users[i])
+}
+
+if(users.length = 3) {
+  console.log("All users loaded")
+}
+
+function add(a, b) {
+    return a + b + c
+}
+
+console.log(result.toUppercase())
